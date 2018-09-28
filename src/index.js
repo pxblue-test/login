23187e5dfc3cb45f4ea7a19d3218fcfac32da634
Joseph Boyle <boyle.p.joseph@gmail.com>
2018-09-27 22:37:56 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App.js';
 import './style.css';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import {EatonThemes} from '@pxblue/themes/react';
+import * as PXBThemes from '@pxblue/themes/react';
 import { Provider } from 'react-redux';
 import {store} from './store/store.js';
 
 ReactDOM.render(
-  <MuiThemeProvider theme={createMuiTheme(EatonThemes.blue)}>
+  <MuiThemeProvider theme={createMuiTheme(PXBThemes.blue)}>
     <Provider store={store}>
       <App />
     </Provider>
