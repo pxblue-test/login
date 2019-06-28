d550b131af034927a746f03499cb14bc2b8e64d1
joebochill <boyle.p.joseph@gmail.com>
2019-06-28 13:24:35 -0400

    Clean up sign in styles

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,23 +1,28 @@
- import React from 'react';
-import { ROUTES } from '../constants/routes';
-import Input from '../components/MatTextfield';
-import {
-    Platform,
-    AsyncStorage,
-    Text,
-    TextInput,
-    View,
-    ImageBackground,
-    Image,
-  } from "react-native";
-import { Card, CheckBox } from "react-native-elements";
-    
-import styles from "../styles";
+import React from 'react';
 
-//   import AsyncStorage from '@react-native-community/async-storage';
-import Button from '../components/MatButton';
-import * as Colors from '@pxblue/colors';
+// Constants
+import { ROUTES } from '../constants/routes';
 import { EMAIL_REGEX } from '../constants/index';
+
+// Components
+import {
+    AsyncStorage,
+    View,
+    Image,
+    StyleSheet,
+    ScrollView
+} from "react-native";
+import { Card, CheckBox } from "react-native-elements";
+//   import AsyncStorage from '@react-native-community/async-storage';
+
+// Custom styled components
+import Button from '../components/MatButton';
+import Input from '../components/MatTextfield';
+import Text from '../components/Typography';
+
+// Styles
+import * as Colors from '@pxblue/colors';
+import styles from "../styles";
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -31,68 +36,77 @@ class SignIn extends React.Component {
     };
 
     render() {
-        const {navigation} = this.props;
+        const { navigation } = this.props;
         const { email, password, canRemember } = this.state;
         return (
-            <View style={{height: '100%', marginLeft: -15, paddingLeft: -15, justifyContent: 'center', backgroundColor: Colors.blue['500']}}>
-                <Card containerStyle={styles.card}>
-                    <View style={{flex: 1}}>
-                        <Image
-                            source={require("../../assets/12.jpg")}
-                            style={styles.logo}
-                        />
-                        <Input 
-                            label={'Email Address*'}
-                            keyboardType={'email-address'}
-                            value={email} 
-                            onChangeText={(newText) => this.setState({email : newText})}
-                            errorMessage={email && !email.match(EMAIL_REGEX) ? 'Invalid Email Address' : ' '}
-                        />
-                        <Input 
-                            label={'Password*'} 
-                            value={password} 
-                            onChangeText={(newText) => this.setState({password : newText})}
-                            containerStyle={{marginBottom: 20}}
-                            secureTextEntry
-                        />
-                        <Button 
-                            title={'Log In'} 
-                            onPress={this._signInAsync}
-                            disabled={!email.match(EMAIL_REGEX) || !password}
-                            color={'primary'}
-                        />
-                        <CheckBox
-                            title="Remember me"
-                            checked={canRemember}
-                            containerStyle={{ backgroundColor: 'transparent', borderWidth: 0}}
-                            onPress={() => this.setState({ canRemember: !canRemember })}
-                        />
-                        <View style={styles.signUpAndPassword}>
+            <View style={{ flex: 1, backgroundColor: Colors.blue['500'] }}>
+                <ScrollView style={{ flex: 1 }} contentContainerStyle={{ flexGrow: 1, justifyContent: 'stretch', alignItems: 'center' }}>
+                    <View style={{ flex: 1, justifyContent: 'center' }}>
+                        <Card containerStyle={{ paddingHorizontal: 50 }}>
+                            <View style={styles.logo_product}>
+                                <Image
+                                    resizeMethod='resize'
+                                    source={require("../../assets/logo.jpg")}
+                                    style={styles.svg}
+                                />
+                            </View>
+                            <Input
+                                label={'Email Address*'}
+                                keyboardType={'email-address'}
+                                value={email}
+                                onChangeText={(newText) => this.setState({ email: newText })}
+                                errorMessage={email && !email.match(EMAIL_REGEX) ? 'Invalid Email Address' : ' '}
+                            />
+                            <Input
+                                label={'Password*'}
+                                value={password}
+                                onChangeText={(newText) => this.setState({ password: newText })}
+                                containerStyle={{ marginBottom: 20 }}
+                                secureTextEntry
+                            />
                             <Button
-                                type={'solid'}
-                                title="Sign Up"
-                                onPress={()=>navigation.push(ROUTES.REGISTER)}
+                                title={'Log In'}
+                                onPress={this._signInAsync}
+                                disabled={!email.match(EMAIL_REGEX) || !password}
                                 color={'primary'}
                             />
-                            <Button 
-                                type={'solid'} 
-                                title="Forgot Password" 
-                                onPress={()=>navigation.push(ROUTES.FORGOT)} 
-                                color={'primary'}
+                            <CheckBox
+                                title="Remember me"
+                                checked={canRemember}
+                                containerStyle={styles.checkbox}
+                                textStyle={{ fontWeight: '400', fontSize: 16 }}
+                                onPress={() => this.setState({ canRemember: !canRemember })}
                             />
-                        </View>
-                        <Image
-                            source={require("../..//assets/Logo_Samples2_91_min.jpg")}
-                            style={styles.logo1}
-                        />
-                        <Text style={styles.termsAndConditions}>
-                            By Logging in you agree to our
-                        </Text>
-                        <Text>
-                            <Text style={styles.link}>Terms and Conditions</Text> and <Text style={styles.link}>Privacy policy </Text>
-                        </Text>
-                    </View>            
-                </Card>      
+                            <View style={StyleSheet.flatten([styles.horizontal_even, { marginTop: 10 }])}>
+                                <Button
+                                    type={'clear'}
+                                    title="Sign Up"
+                                    onPress={() => navigation.push(ROUTES.REGISTER)}
+                                    color={'primary'}
+                                />
+                                <Button
+                                    type={'clear'}
+                                    title="Forgot Password"
+                                    onPress={() => navigation.push(ROUTES.FORGOT)}
+                                    color={'primary'}
+                                />
+                            </View>
+                            <View style={styles.logo_cyber}>
+                                <Image
+                                    resizeMethod='resize'
+                                    source={require("../../assets/cyber.jpg")}
+                                    style={styles.svg}
+                                />
+                            </View>
+                            <Text style={{ textAlign: 'center' }}>
+                                By Logging in you agree to our
+                            </Text>
+                            <Text style={{ textAlign: 'center' }}>
+                                <Text style={styles.link}>Terms and Conditions</Text> and <Text style={styles.link}>Privacy policy </Text>
+                            </Text>
+                        </Card>
+                    </View>
+                </ScrollView>
             </View>
         );
     }
--- src/styles.js
+++ src/styles.js
@@ -2,95 +2,31 @@ import { StyleSheet } from "react-native";
 import * as Colors from '@pxblue/colors';
 
 export default StyleSheet.create({
-  background: {
-    width: "100%",
-    height: "100%",
-
+  checkbox: {
+    backgroundColor: 'transparent',
+    borderWidth: 0,
+    alignSelf: 'center'
   },
-  card: {
-    height: "65%",
-    width: "100%",
-    alignItems: "center",
-    marginRight: 100,
-
-  },
-  mainContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-
-  },
-  logo: {
-    width: 200,
-    height: 100,
-    resizeMode: "contain",
-    alignSelf: "center"
-  },
-  logo1: {
-    width: 60,
-    height: 60,
-    alignSelf: "center",
-    resizeMode: "contain"
-  },
-
-
-  emailAddressAndPassword: {
-    flex: 1,
-    justifyContent: "space-between",
-    flexDirection: "column",
-    margin: "-1px 0px 0px",
-    padding: "7px 0px"
-  },
-  emailAddress: {
-    color: "blue"
-  },
-  Password: {
-    color: "blue"
-  },
-
-
-
-  footer: {
-    flex: 1,
-    justifyContent: "space-between",
-    flexDirection: "row",
-  },
-  loginButton: {
-    alignSelf: "center",
-  },
-
-  rememberMe: {
-    alignSelf: "center",
-    fontSize: 20
-  },
-
-  signUpAndPassword: {
-    flex: 1,
+  horizontal_even: {
     justifyContent: "space-evenly",
     flexDirection: "row",
-    marginTop: 10
-  },
-  signUp: {
-    color: "blue",
-    fontSize: 20,
-  },
-  forgotPassword: {
-    color: "blue",
-    fontSize: 20,
-  },
-  termsAndConditions: {
-    textAlign: "center"
   },
   link: {
     color: Colors.blue['500'],
   },
-  registrationPage: {
-    flex: 1,
+  logo_product: {
+    height: 75,
+    width: '100%',
+    marginBottom: 5
   },
-  heading: {
-    fontSize: 20,
-    color: '#4da3d4',
-    fontWeight: '700',
-    marginBottom: 20,
+  logo_cyber: {
+    height: 75,
+    width: '100%',
+    marginVertical: 20
+  },
+  svg: {
+    width: '100%',
+    height: '100%',
+    resizeMode: "contain",
   }
 });
