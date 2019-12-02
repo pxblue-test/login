ccf8fc3c1dc239917af5983c12a1ee60b7b73113
huayunh <hhuayun0828@gmail.com>
2019-12-02 11:08:34 -0500

    clear jest setup

--- jestSetup.js
+++
@@ -1,4 +0,0 @@
-// NOTE: uncomment the two lines below in your production code.
-
-// import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
-// jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
